6d95cfa4a8bbc09274dbaa37c4b23d7d5e88ddd7
MICHELE SANTANA <78629902+micheleclarice@users.noreply.github.com>
2022-03-03 14:08:31 -0300

    Add files via upload

---
+++ TodoContext .cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APP TAREFAS.models
+{
+    public class TodoContext 
+    {
+        
+    }
+}
