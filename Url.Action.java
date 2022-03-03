aced465f3831584fcc6842c69fc76b70098f8f2b
MICHELE SANTANA <78629902+micheleclarice@users.noreply.github.com>
2022-03-03 20:39:31 -0300

    Add files via upload

---
+++ Url.Action.java
@@ -0,0 +1,5 @@
+ambient values: { controller = "UrlGeneration", action = "Source" }
+values passed to Url.Action: { controller = "UrlGeneration", action = "Destination" }
+route template: {controller}/{action}/{id?}
+
+result: /UrlGeneration/Destination
